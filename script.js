85af8c92d4be5b8aba6260c1aea1694a967bcd33
Neha Gurnani <146248620+nehag2005@users.noreply.github.com>
2024-03-03 14:27:17 +0000

    Nav bar

---
+++ script.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("MySideNav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("MySideNav").style.width = "0px";
+    document.getElementById("main").style.marginLeft = "0px";
+  }
+  
